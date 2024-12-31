465a8555ab97cb3a71679c9f847901941e702033
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-31 18:03:50 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Yue-Bot ✰');
+console.log('\n🚀 Iniciando Génesis-Ultra...');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
