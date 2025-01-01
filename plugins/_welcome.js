07e19e531d954c8443f8e2b937a25d6492eae014
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-01 17:13:27 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/Tdxwk.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
   
@@ -13,17 +13,17 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let userName = user ? user.name : await conn.getName(who);
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `ゲ◜៹ New Member ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]} \nGrupo: ${groupMetadata.subject}\n${dev}`;
+    let bienvenida = `ゲ◜៹ Bienvenid@ ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]} \nGrupo: ${groupMetadata.subject}\n${dev}`;
     
 await conn.sendMini(m.chat, packname, dev, bienvenida, img, img, channel, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `ゲ◜៹ Bye Member ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]}\nGrupo: ${groupMetadata.subject}\n${dev}`;
+    let bye = `ゲ◜៹ Adiós ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]}\nGrupo: ${groupMetadata.subject}\n${dev}`;
 await conn.sendMini(m.chat, packname, dev, bye, img, img, channel, estilo)
   }
   
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `ゲ◜៹ Bye Member ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]}\nGrupo: ${groupMetadata.subject}\n${dev}`;
+    let kick = `ゲ◜៹ Adiós ៹◞ゲ \n\nUsuario: @${m.messageStubParameters[0].split`@`[0]}\nGrupo: ${groupMetadata.subject}\n${dev}`;
 await conn.sendMini(m.chat, packname, dev, kick, img, img, channel, estilo)
 }}
