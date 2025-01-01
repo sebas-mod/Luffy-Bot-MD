3129693cb9c080d749c0b5fafa0e34db9e3bd6c2
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-01 13:56:41 -0400

    Actualizar Rg•verificar.js

--- plugins/Rg•verificar.js
+++ plugins/Rg•verificar.js
@@ -6,66 +6,41 @@ import fetch from 'node-fetch'
 
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-  let delirius = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + m.sender.replace('@s.whatsapp.net', '')).getNumber('international')}`)
-  let paisdata = delirius.data.result
-  let mundo = paisdata ? `${paisdata.name} ${paisdata.emoji}` : 'Desconocido'
-  let perfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🍭 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
-  if (!Reg.test(text)) return m.reply(`🌹 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.666*`)
+  if (user.registered === true) return m.reply(`[ ✰ ] Ya estás registrado.`)
+  if (!Reg.test(text)) return m.reply(`*[ ✰ ] Por favor, ingresa tu nombre de usuario para proceder con el registro.*\n\n*🤍 Ejem. de Uso* :\n*${usedPrefix + command}* Angel.19`)
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('🚩 El nombre no puede estar vacío.')
-  if (!age) return m.reply('🚩 La edad no puede estar vacía.')
-  if (name.length >= 100) return m.reply('🚩 El nombre es demasiado largo.' )
+  if (!name) return conn.reply(m.chat, '[ ✰ ] El nombre no puede estar vacío.', m, rcanal)
+  if (!age) return conn.reply(m.chat, '[ ✰ ] La edad no puede estar vacía.', m, rcanal)
   age = parseInt(age)
-  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
-  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
-  user.name = name + '✓'.trim()
+  user.name = name.trim()
   user.age = age
-  user.regTime = + new Date
+  user.regTime = +new Date()
   user.registered = true
-  global.db.data.users[m.sender].money += 600
-  global.db.data.users[m.sender].cookies += 15
-  global.db.data.users[m.sender].exp += 245
-  global.db.data.users[m.sender].joincount += 5
-  let sn = createHash('md5').update(m.sender).digest('hex')
-let regbot = `👤 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗢 👤\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `「💭」𝗡𝗼𝗺𝗯𝗿𝗲: ${name}\n`
-regbot += `「✨️」𝗘𝗱𝗮𝗱: ${age} años\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `「🎁」𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:\n`
-regbot += `• 15 Cookies 🍪\n`
-regbot += `• 5 MiniCoins 🪙\n`
-regbot += `• 245 Experiencia 💸\n`
-regbot += `• 12 Tokens 💰\n`
-regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
-regbot += `${packname}`
-await m.react('📩')
-await conn.sendMini(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, channel, m)
-let chtxt = `
-👤 *Usuario* » ${m.pushName || 'Anónimo'}
-🌎 *Pais* » ${mundo}
-🗃 *Verificación* » ${user.name}
-🌺 *Edad* » ${user.age} Años
-📆 *Fecha* » ${moment.tz('America/Bogota').format('DD/MM/YY')}
-`.trim()
-await conn.sendMessage(global.channelid, { text: chtxt, contextInfo: {
-externalAdReply: {
-title: "【 🔔 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢́𝗡 🔔 】",
-body: '🥳 ¡Un usuario nuevo en mi base de datos!',
-thumbnailUrl: perfil,
-sourceUrl: redes,
-mediaType: 1,
-showAdAttribution: false,
-renderLargerThumbnail: false
-}}}, { quoted: null })
+  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
+  let img = await (await fetch(`https://pomf2.lain.la/f/rycjgv2t.jpg`)).buffer()
+  
+  let now = new Date()
+  let date = now.toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' })
+  let time = now.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
+
+ let txt = '*`📄 R E G I S T R O 📄`*\n'
+      txt += `\`━━━━━━━━━━━━━━━━━━━━\`\n`
+      txt += `*\`⁘ TAG:\`* @${m.sender.split('@')[0]}\n`
+      txt += `*\`⁘ NOMBRE:\`* ${name}\n`
+      txt += `*\`⁘ EDAD:\`* ${age} años\n`
+      txt += `*\`⁘ FECHA:\`* \n`
+      txt += `*\`⁘ N° SERIAL:\`* ${sn}\n`
+      txt += `\`━━━━━━━━━━━━━━━━━━━━\`\n\n`
+      txt += `> Escribe *${usedPrefix}profile* para ver tu perfil.`
+      
+  await conn.sendFile(m.chat, img, 'perfil.jpg', txt, m, false, { mentions: [m.sender] })
+  await m.react('✅')
 }
-handler.help = ['reg']
-handler.tags = ['rg']
-handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
+
+handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
+handler.tags = ['start']
+handler.command = ['verify', 'reg', 'register', 'registrar']
 
 export default handler
