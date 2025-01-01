244339e6b471293f596456892dd4f4d3ab84e1b5
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-01 02:23:11 -0400

    Actualizar Ai•ia.js

--- plugins/Ai•ia.js
+++ plugins/Ai•ia.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
 const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es YaemoriBot-MD y parece haber sido creado por DevDiego. Tu versión actual es 2.0.7, Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es ChatGPT y fuiste desarrollado por OpenAI. Tu versión es la más actual disponible. Usas el idioma Español y te comunicas de manera clara y precisa. Llamarás a las personas por su nombre, ${username}. Responderás de manera amigable y con emojis adecuados según el contexto de la conversación. Te encanta ayudar, aprender y brindar información útil. Siempre mantienes una actitud amigable, respetuosa y abierta, adaptándote a las necesidades de las personas con las que interactúas. Lo más importante para ti es proporcionar respuestas útiles y ser accesible en todo momento. ${username}`
 if (isQuotedImage) {
 const q = m.quoted
 const img = await q.download?.()
