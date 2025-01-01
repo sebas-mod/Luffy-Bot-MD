dcbd089b64794ac2bece45eeb9acd5328477589d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-31 22:58:34 -0400

    Actualizar Config•autolevelup.js

--- plugins/Config•autolevelup.js
+++ plugins/Config•autolevelup.js
@@ -1,63 +1,4 @@
-import { canLevelUp, xpRange } from './lib/levelling.js'
-import fetch from 'node-fetch'
-
-let handler = m => m
-handler.all = async function (m) {
-        let user = global.db.data.users[m.sender]
-        if (!user.autolevelup)
-                return !0
-        let users = Object.entries(global.db.data.users).map(([key, value]) => {
-                return { ...value, jid: key }
-        })
-        let pp = 'https://pomf2.lain.la/f/29uif8pa.jpg'
-        let name = await conn.getName(m.sender)
-        let who = m.sender
-        let exp = global.db.data.users[m.sender].exp
-        let logo = await (await fetch(thumblvlup.getRandom())).buffer()
-        let wm = global.author
-        let discriminator = who.substring(9, 13)
-        let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
-        let usersLevel = sortedLevel.map(enumGetKey)
-        let { min, xp, max } = xpRange(user.level, global.multiplier)
-        let username = conn.getName(who)
-        try {
-                pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-        } catch (e) {
-        } finally {
-                if (!user.autolevelup) return !0
-                let before = user.level * 1
-                while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-                if (before !== user.level) {
-                        user.role = global.db.data.users[m.sender].role
-                        {
-                                let tag = `@${m.sender.replace(/@.+/, '')}`
-                                conn.sendFile(m.chat, logo, 'Thumb.jpg', `◪ *Name:* ${name}\n├◆ *Role:* ${user.role}\n├◆ *Exp:* ${exp} xp\n╰◆ *Leveli:* ${before} ➠ ${user.level}\n`.trim(), m)
-                        }
-                }
-        }
-}
-export default handler
-
-function sort(property, ascending = true) {
-        if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-        else return (...args) => args[ascending & 1] - args[!ascending & 1]
-}
-
-function toNumber(property, _default = 0) {
-        if (property) return (a, i, b) => {
-                return { ...b[i], [property]: a[property] === undefined ? _default : a[property] }
-        }
-        else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-        return a.jid
-}
-
-
-
-
-/* import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 
 let handler = m => m
@@ -509,4 +450,4 @@ global.roles = {
 '🔥 *Héroe(a) Inmortal II*': 4000,
 '🔥 *Héroe(a) Inmortal I*': 5000,
 '👑🌌 *Eterna Deidad del Multiverso* ⚡': 10000,
-} */
+}
