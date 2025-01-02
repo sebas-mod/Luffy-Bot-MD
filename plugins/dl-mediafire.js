622dd4a3d605145bc1504d6b848ad502be637dc1
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-01 21:10:12 -0400

    Actualizar dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de mediafire`, m)
+    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de mediafire`, m, null, fake)
 
     try {
         let api = await fetch(`https://restapi.apibotwa.biz.id/api/mediafire?url=${text}`)
