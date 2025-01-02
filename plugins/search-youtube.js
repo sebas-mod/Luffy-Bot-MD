9f3cbc483a12702abdfae8b1ea74ba9dc68809dd
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-02 17:45:55 -0400

    Actualizar search-youtube.js

--- plugins/search-youtube.js
+++ plugins/search-youtube.js
@@ -101,7 +101,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   await m.react('✅')
 }
 
-handler.help = ["ytsearch", "yts"];
+handler.help = ["ytsearch *<texto>*", "yts *<texto>*"];
 handler.tags = ["search"];
 handler.command = ["ytsearch", "yts"];
 
