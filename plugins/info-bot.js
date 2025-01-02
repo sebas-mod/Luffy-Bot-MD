725a510cae003a9669c6f36df940ad0fcd71ab44
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-02 16:52:09 -0400

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,15 +1,13 @@
 import fs from 'fs';
 const handler = (m) => m;
 handler.all = async function(m) {
+  const vn = './media/audios/bot.mp3';
+  const chat = global.db.data.chats[m.chat];
 
-const chat = global.db.data.chats[m.chat];
-if (chat.isBaneed) return
-if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🚩 ¡Hola! Soy Ai Yaemori, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+  if (/^bot$/i.test(m.text) && !chat.isBanned) {
+    conn.sendPresenceUpdate('recording', m.chat);
+conn.reply(m.chat, `🤍 ¡Hola! Soy Ai Genesis, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
-/*if (/^corin|corín|corinplus|corínplus|corinplushost|corínplushost|plus$/i.test(m.text)) {
-conn.reply(m.chat, `🚀 CorinPlus Hosting ¡El plus que necesitas!\n🚩 *Dash:* https://dash.corinplus.com\n🌱 *Panel:* https://ctrl.corinplus.com`, m, rcanal, )
-}*/
 if (/^sexo$/i.test(m.text)) {
 conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
 }
@@ -22,6 +20,6 @@ conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
 if (/^pene$/i.test(m.text)) {
 conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
 }
-return !0;
+  return !0;
 };
 export default handler;
