2e3406533c64562c51967d44a45fa0f941861bd6
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-02 17:52:43 -0400

    Actualizar grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,13 +1,12 @@
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
-conn.reply(m.chat, `🚩 *Este chat fue baneado con éxito*`, m, rcanal)
+conn.reply(m.chat, `🤍 *Este chat fue baneado con éxito*`, m, rcanal)
 
 }
 handler.help = ['banchat']
-handler.tags = ['grupo']
+handler.tags = ['group']
 handler.command = ['banchat']
-
 handler.botAdmin = true
 handler.admin = true 
 handler.group = true
