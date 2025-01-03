4be40f40e4c6994bda013d27faab982eefecd378
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-02 22:09:40 -0400

    Actualizar serbot.js

--- plugins/serbot.js
+++ plugins/serbot.js
@@ -67,7 +67,7 @@ resultado += segundos + " segundos";
 }
 return resultado;
 }
-const message = users.map((v, index) => `• 「 ${index + 1} 」\n📎 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado\n👤 Usuario: ${v.user.name || 'Sub-Bot'}\n🕑 Online: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}`).join('\n\n__________________________\n\n');
+const message = users.map((v, index) => `• 「 ${index + 1} 」${v.user.name || 'Sub-Bot'}\n📎 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}menu\n🕑 Online: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}`).join('\n\n__________________________\n\n');
 const replyMessage = message.length === 0 ? `No hay Sub-Bots disponible por el momento, verifique mas tarde.` : message;
 const totalUsers = users.length;
 const responseMessage = `🤍 *LISTA DE SUBBOTS*\n\nSubBots: ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
