665057765f45739d15c7c4b58f32b7af73c4328d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2025-01-02 21:02:16 -0400

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,4 +1,31 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i;
+
+let handler = m => m;
+handler.before = async function (m, { conn, isOwner, isROwner }) {
+    if (m.fromMe) return true;
+    if (m.isGroup) return false;
+    if (!m.message) return true;
+
+    const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i');
+    if (regexWithPrefix.test(m.text.toLowerCase().trim())) return true;
+
+    let chat, user, bot, mensaje;
+    chat = global.db.data.chats[m.chat];
+    user = global.db.data.users[m.sender];
+    bot = global.db.data.settings[this.user.jid] || {};
+
+    if (bot.antiPrivate && !isOwner && !isROwner) {
+        return false;
+    }
+
+    return false;
+};
+export default handler;
+
+
+
+
+/* export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
@@ -11,4 +38,4 @@ if (m.chat === '120363310433406751@newsletter') return !0
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
-}
+} */
