c4476c013e3169bb8bb999991f08b1811f0558e7
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-04 18:26:42 -0400

    Actualizar test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -7,9 +7,11 @@ if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 try {
 let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp3?url=${text}`)
 let json = await api.json()
+
 let { title, image, url } = json.result
 // let title = json.result.metadata.title
 // let dl_url = json.result.download.url
+await m.react('✅');
 await conn.sendMessage(m.chat, { audio: { url: url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
 
 } catch (error) {
