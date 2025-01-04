a5af1e05f39a69be57568647d9e13efe477b8269
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-04 13:59:18 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,26 +1,23 @@
 import { sticker } from '../lib/sticker.js'
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import { webp2png } from '../lib/webp2mp4.js'
+//import uploadFile from '../lib/uploadFile.js'
+//import uploadImage from '../lib/uploadImage.js'
+//import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-let name = conn.getName(m.sender);
-let text1 = `Genesis-Ultra\nAuthor:\nFecha:\nCreador:`
-let text2 = `${vs}\n${name}\n${fecha}\nIzumi.kzx`
 let stiker = false
 try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`☁️ *¡El video no puede durar mas de 8 segundos!*`)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply(`《✧》¡El video no puede durar mas de 10 segundos!`)
 let img = await q.download?.()
 
-if (!img) return conn.reply(m.chat, `⚠️ *_La conversión ha fallado, intenta enviar primero imagen/video/gif y luego responde con el comando._*`, m, rcanal)
+if (!img) return conn.reply(m.chat, `《✧》Por favor, envia una imagen o video para hacer un sticker`, m, rcanal)
 
 let out
 try {
-stiker = await sticker(img, false, text1, text2)
+stiker = await sticker(img, false, global.packsticker, global.author)
 } catch (e) {
 console.error(e)
 } finally {
@@ -29,26 +26,28 @@ if (/webp/g.test(mime)) out = await webp2png(img)
 else if (/image/g.test(mime)) out = await uploadImage(img)
 else if (/video/g.test(mime)) out = await uploadFile(img)
 if (typeof out !== 'string') out = await uploadImage(img)
-stiker = await sticker(false, out, text1, text2)
+stiker = await sticker(false, out, global.packsticker, global.author)
 }}
 } else if (args[0]) {
-if (isUrl(args[0])) stiker = await sticker(false, args[0], text1, text2)
+if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packsticker, global.author)
 
-else return m.reply(`💫 El url es incorrecto`)
+else return m.reply(`《✧》El Link Es Incorrecto`)
 
 }
 } catch (e) {
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `Genesis Bot - MD 🤍`, body: `✡︎ Sticker By • Izumi.kzx`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: botname, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
 
-else return conn.reply(m.chat, '⚠️ *_La conversión ha fallado, intenta enviar primero imagen/video/gif y luego responde con el comando._*', m, rcanal)
+else return conn.reply(m.chat, '《✧》Por favor, envia una imagen o video para hacer un sticker', m, rcanal)
 
 
 }}
-handler.help = ['stiker *<img>*', 'sticker *<url>*']
+handler.help = ['stiker <img>', 'sticker <url>']
 handler.tags = ['sticker']
+handler.group = false;
+handler.register = true
 handler.command = ['s', 'sticker', 'stiker']
 
 export default handler
