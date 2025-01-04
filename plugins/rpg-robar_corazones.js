d5d4ed1795af71c1b2b574a7a2023b91f42def00
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-03 21:49:12 -0400

    Actualizar rpg-robar_corazones.js

--- plugins/rpg-robar_corazones.js
+++ plugins/rpg-robar_corazones.js
@@ -2,26 +2,26 @@ const ro = 30;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob2 + 7200000;
   if (new Date - global.db.data.users[m.sender].lastrob2 < 7200000) {
-  conn.reply(m.chat, `*🚩 Hey! Espera ${msToTime(time - new Date())} para volver a robar*`, m, rcanal);
+  conn.reply(m.chat, `*☁️ Hey! Espera ${msToTime(time - new Date())} para volver a robar*`, m, rcanal);
   return;
   }
   let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   if (!who) {
-  conn.reply(m.chat, `*🚩 Etiqueta al usuario.*`, m, fake)
+  conn.reply(m.chat, `*☁️ Etiqueta al usuario.*`, m, fake)
   return;
     };
   if (!(who in global.db.data.users)) { 
-  conn.reply(m.chat, `*🚩 El usuario no se encuentra en mi base de datos.*`, m, rcanal)
+  conn.reply(m.chat, `*☁️ El usuario no se encuentra en mi base de datos.*`, m, rcanal)
 return;
   }
   const users = global.db.data.users[who];
   const rob = Math.floor(Math.random() * ro);
-  if (users.cookies < rob) return conn.reply(m.chat, `😔 @${who.split`@`[0]} Tiene menos de *${ro} Cookies 🍪*\nNo robes a un pobre :v`, m, {mentions: [who]});
+  if (users.corazones < rob) return conn.reply(m.chat, `😔 @${who.split`@`[0]} Tiene menos de *${ro} Corazones 🤍*\nNo robes a un pobre :v`, m, {mentions: [who]});
   global.db.data.users[m.sender].cookies += rob;
-  global.db.data.users[who].cookies -= rob;
-  conn.reply(m.chat, `*🚩 Robastes ${rob} Cookies 🍪 a @${who.split`@`[0]}*`, m, {mentions: [who]});
+  global.db.data.users[who].corazones -= rob;
+  conn.reply(m.chat, `*☁️ Robastes ${rob} Corazones 🤍 a @${who.split`@`[0]}*`, m, {mentions: [who]});
   global.db.data.users[m.sender].lastrob2 = new Date * 1;
 };
 handler.help = ['rob2'];
