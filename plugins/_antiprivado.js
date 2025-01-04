053ad2363fab09a6c373d6ba68ffde996437ca75
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-03 21:40:20 -0400

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,31 +1,4 @@
-const comandos = /piedra|papel|tijera|code|jadibot --code|--code|owner|grupos|bots|deletebot|eliminarsesion|serbot|reg|jadibot/i;
-
-let handler = m => m;
-handler.before = async function (m, { conn, isOwner, isROwner }) {
-    if (m.fromMe) return true;
-    if (m.isGroup) return false;
-    if (!m.message) return true;
-
-    const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i');
-    if (regexWithPrefix.test(m.text.toLowerCase().trim())) return true;
-
-    let chat, user, bot, mensaje;
-    chat = global.db.data.chats[m.chat];
-    user = global.db.data.users[m.sender];
-    bot = global.db.data.settings[this.user.jid] || {};
-
-    if (bot.antiPrivate && !isOwner && !isROwner) {
-        return false;
-    }
-
-    return false;
-};
-export default handler;
-
-
-
-
-/* export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
@@ -34,8 +7,8 @@ export default handler;
   const bot = global.db.data.settings[this.user.jid] || {};
 if (m.chat === '120363310433406751@newsletter') return !0
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`《✧》Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\n${grupo}`, false, {mentions: [m.sender]});
+    await m.reply(`《✧》Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\nhttps://chat.whatsapp.com/GqKwwoV2JJaJDP2SL7SddX`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
-} */
+}
