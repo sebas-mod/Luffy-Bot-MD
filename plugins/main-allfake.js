1eb551aeb071d8d2db9f19ad55e4213ce9893edf
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-04 16:10:30 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -90,8 +90,9 @@ global.nombre = m.pushName || 'Anónimo'
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
-global.sticker1 = `☁️Usuario: ${nombre}\n ☁️ Creador:`;
-global.sticker2 = `☁️Bot: ${botname}\nIzumi.kzx`
+global.sticker1 = `☁️Usuario: ${nombre}
+☁️ Creador: Izumi-kzx`;
+global.sticker2 = `☁️Bot: ${botname}`
 
 //Fakes
 global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
