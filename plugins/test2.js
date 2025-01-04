4100efa9a46e320c3872e5a076805112dff3adbc
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-04 19:33:16 -0400

    Actualizar test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -1,62 +1,45 @@
-import fetch from 'node-fetch';
+/* 
+*❀ By Jtxs*
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+// *[ ❀ YTMP3 ]*
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m);
-  }
-
-  // Verifica si el enlace de YouTube es válido
-  const urlRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|v\/|e\/|u\/\w\/|.+\/videoseries\?v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[^\s]*)/;
-  if (!urlRegex.test(text)) {
-    return conn.reply(m.chat, `❀ El link de YouTube es inválido.`, m);
-  }
+  if (!text) return conn.reply(m.chat, `❀ Ingresa un link de YouTube`, m)
 
   try {
-    // Realiza la solicitud a la API externa
-    let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp3?url=${text}`);
-    let json = await api.json();
+    let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/ytmp3?url=${text}`)
+    let json = await api.json()
 
-    if (!json.result) {
-      return conn.reply(m.chat, `❀ No se pudo obtener el archivo de audio de YouTube.`, m);
-    }
+    if (!json.status) return conn.reply(m.chat, `❀ Ocurrió un error al procesar el enlace.`, m)
 
-    let { title, imagen, description, timestamp, ago, views, author } = json.result;
+    let title = json.data.download.filename || 'audio'
+    let dl_url = json.data.download.url
 
-    // Verificar si 'imagen' y 'dl_url' son válidos
-    if (!imagen || !json.result.download || !json.result.download.url) {
-      return conn.reply(m.chat, `❀ Error: URL de imagen o de descarga no disponible.`, m);
-    }
-
-    // Obtener la imagen como buffer
-    let img = await (await fetch(imagen)).buffer();
-    let dl_url = json.result.download.url;
-    let quality = json.result.download.quality;
-
-    await m.react('☁️');
-
-    // Enviar como documento
-    await conn.sendMessage(m.chat, {
-      document: { url: dl_url },
-      fileName: `${title}.mp3`,
-      fileLength: quality,
-      caption: `❀ ${title}`,
-      mimetype: 'audio/mpeg',
-      jpegThumbnail: img,
-    }, { quoted: m });
-
-    // Enviar como audio
     await conn.sendMessage(m.chat, { 
       audio: { url: dl_url }, 
-      fileName: `${title}.mp3`, 
+      fileName: `${title}`, 
       mimetype: 'audio/mp4' 
-    }, { quoted: m });
+    }, { quoted: m })
 
   } catch (error) {
-    console.error(error);
-    conn.reply(m.chat, `❀ Ocurrió un error al procesar la solicitud.`, m);
+    console.error(error)
+    conn.reply(m.chat, `❀ Ocurrió un error al procesar el enlace.`, m)
   }
-};
+}
 
-handler.command = ['ytmp3v2'];
+handler.command = ['ytmp3']
 
-export default handler;
+export default handler
