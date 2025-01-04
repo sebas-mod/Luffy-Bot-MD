c2246ef28884b57efbe0e6ddc637930b13e81d12
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-03 21:45:13 -0400

    Actualizar rpg-depositar.js

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -2,22 +2,22 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
 let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *🍪 Cookies* que deseas Depositar.')
-if ((args[0]) < 1) return m.reply('🚩 Ingresa una cantidad válida de *🍪 Cookies*.')
+if (!args[0]) return m.reply('☁️ Ingresa la cantidad de *🤍 Corazones* que deseas Depositar.')
+if ((args[0]) < 1) return m.reply('☁️ Ingresa una cantidad válida de *🤍 Corazones*.')
 if (args[0] == 'all') {
-let count = parseInt(user.cookies)
-user.cookies -= count * 1
+let count = parseInt(user.corazones)
+user.corazones -= count * 1
 user.bank += count * 1
-await m.reply(`Depositaste *${count} 🍪 Cookies* al Banco.`)
+await m.reply(`Depositaste *${count} 🤍 Corazones* al Banco.`)
 return !0
 }
-if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
+if (!Number(args[0])) return m.reply('☁️ La cantidad deve ser un Numero.')
 let count = parseInt(args[0])
-if (!user.cookies) return m.reply('No tienes *🍪 Cookies* en la Cartera.')
-if (user.estrellas < count) return m.reply(`Solo tienes *${user.cookies} 🍪 Cookies* en la Cartera.`)
-user.cookies -= count * 1
+if (!user.corazones) return m.reply('No tienes *🤍 Corazones* en la Cartera.')
+if (user.estrellas < count) return m.reply(`Solo tienes *${user.corazones} 🤍 Corazones* en la Cartera.`)
+user.corazones -= count * 1
 user.bank += count * 1
-await m.reply(`Depositaste *${count} 🍪 Cookies* al Banco.`)}
+await m.reply(`Depositaste *${count} 🤍 Corazones* al Banco.`)}
 
 handler.help = ['depositar']
 handler.tags = ['rpg']
