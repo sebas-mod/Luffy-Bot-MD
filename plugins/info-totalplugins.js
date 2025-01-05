6fbe821f44a4068ef02f53dbed5a88aca595f39a
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-05 01:52:08 -0400

    Actualizar plugins

---
+++ plugins/info-totalplugins.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+
+var handler = async (m, { conn, args, command }) => {
+    let totalf = Object.values(global.plugins).filter(
+        (v) => v.help && v.tags
+    ).length;
+
+    let replyMessage = `Total Fitur Bot Saat ini: ${totalf}\n`;
+
+    await conn.reply(m.chat, replyMessage, m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
+                mediaType: 2,
+                description: 'anu',
+                title: packname,
+                body: 'Total Plugins',
+                previewType: 0,
+                thumbnail: "https://files.catbox.moe/d23slh.jpeg",
+                sourceUrl: canal
+            }
+        }
+    });
+}
+
+handler.help = ['totalfitur']
+handler.tags = ['info']
+handler.command = ['totalfitur']
+export default handler
