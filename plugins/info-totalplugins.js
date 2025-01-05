2e07705673ed253a1e39e5cf1d6efc3dace8c742
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-05 01:59:29 -0400

    Actualizar info-totalplugins.js

--- plugins/info-totalplugins.js
+++ plugins/info-totalplugins.js
@@ -1,29 +1,12 @@
-import fs from 'fs'
-
-var handler = async (m, { conn, args, command }) => {
-    let totalf = Object.values(global.plugins).filter(
-        (v) => v.help && v.tags
-    ).length;
-
-    let replyMessage = `Total Fitur Bot Saat ini: ${totalf}\n`;
-
-    await conn.reply(m.chat, replyMessage, m)
-        contextInfo: {
-            externalAdReply: {
-                mediaUrl: '',
-                mediaType: 2,
-                description: dev,
-                title: packname,
-                body: 'Total Plugins',
-                previewType: 0,
-                thumbnail: "https://files.catbox.moe/d23slh.jpeg",
-                sourceUrl: canal
-            }
-        }
-    });
+let handler = async (m, { conn }) => {
+let totalf = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+conn.reply(m.chat, `*🍭 Total de Funciones* : ${totalf}`,m)
 }
 
-handler.help = ['totalp']
-handler.tags = ['info']
-handler.command = ['totalp']
-export default handler
+handler.help = ['totalfunciones']
+handler.tags = ['main']
+handler.command = ['totalfunciones']
+handler.register = true
+export default handler 
