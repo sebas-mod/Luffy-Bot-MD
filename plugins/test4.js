96790c8c5126a9e3f07b24583e35431fe62fa061
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-05 10:47:08 -0400

    Actualizar test4.js

--- plugins/test4.js
+++ plugins/test4.js
@@ -1,30 +1,24 @@
-let handler = async (m, { conn, text, isROwner, isOwner }) => {
-let resp
-const bot = global.db.data.bot[conn.user.jid] || {}
-const chats = bot.chats || {}
-const groups = chats.groups || {}
-const chat = groups[m.chat] || {}
+let handler = async (m, { conn, isRowner }) => {
+    const newName = m.text.trim().split(' ').slice(1).join(' ');
+  
+   
+    if (!newName) {
+      return m.reply('Por favor, proporciona un nuevo nombre para el bot.');
+    }
+  
+   
+    global.botname = newName;  
+  
+    
+    m.reply(`ยกEl nombre del bot ha sido actualizado a: ${newName}!`);
+  
+  
+  };
+  
+ 
+  handler.help = ['setname'];  
+  handler.tags = ['banner'];
+  handler.command = ['setname']; 
+  handler.rowner = true
 
-if (text) {
-chat.sWelcome = text
-resp = '*[❗] MENSAJE DE BIENVENIDA CONFIGURADO CORRECTAMENTE PARA ESTE GRUPO*'
-} else {
-resp = `*[❗] INGRESE EL MENSAJE DE BIENVENIDA QUE DESEE AGREGAR, USE:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`
-}
-let txt = '';
-let count = 0;
-for (const c of resp) {
-await new Promise(resolve => setTimeout(resolve, 1));
-txt += c;
-count++;
-if (count % 10 === 0) {
-await conn.sendPresenceUpdate('composing' , m.chat);
-}
-}
-return conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
-}
-handler.help = ['setwelcome <text>']
-handler.tags = ['group']
-handler.command = ['setwelcome'] 
-handler.group = handler.admin = true
-export default handler
+  export default handler;
