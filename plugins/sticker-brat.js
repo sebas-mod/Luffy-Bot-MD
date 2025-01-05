68b4c91c727193c59d792eef2b5d953c98d4087d
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-05 10:39:56 -0400

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -126,7 +126,7 @@ const handler = async (m, {
 
     try {
         const buffer = await fetchSticker(text);
-        const outputFilePath = path.join(tmpdir(), `sticker-${Date.now()}.webp`, false, global.packname, global.author);
+        const outputFilePath = path.join(tmpdir(), `sticker-${Date.now()}.webp`);
         await sharp(buffer)
             .resize(512, 512, {
                 fit: 'contain',
