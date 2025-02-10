d180ae750b3b920e4d6c6bf17264e39ccd735131
Sebas-MD <luisrojas30921@gmail.com>
2025-02-10 14:37:20 -0300

    Create descargas-cocofiun.js

---
+++ plugins/descargas-cocofiun.js
@@ -0,0 +1,33 @@
+import fetch from "node-fetch"
+
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+    if (!args[0]) {
+        await m.react('✖️')
+        return m.reply("ingresa un link de cocofun")
+    }
+    
+    try {
+        await m.react('🕒') 
+        let api = await fetch(`https://api.agatz.xyz/api/cocofundl?url=${args[0]}`)
+        let json = await api.json()
+        let { title, description, image, video, topic, caption, play, like, share, duration, thumbnail, watermark, no_watermark } = json.data
+        let txt = `*Title :* ${title}
+*Descripcion :* ${description}
+*Visitas :* ${play}
+*Likes :* ${like}
+*Duracion :* ${duration}
+`
+        await conn.sendFile(m.chat, image, 'image.jpg', txt, m)
+        await conn.sendFile(m.chat, no_watermark, 'video.mp4', txt, m)
+        await m.react('✅')
+    } catch (error) {
+        console.error(error)
+        await m.react('❌')
+    }
+}
+
+handler.help = ['cocofundl *<url>*'];
+handler.tags = ['dl'];
+handler.command = ['cocofundl']
+
+export default handler
