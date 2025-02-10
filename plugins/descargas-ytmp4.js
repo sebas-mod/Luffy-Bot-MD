a4a7af93cb4169b8fc0b7017f068b5b3af3b18ba
Sebas-MD <luisrojas30921@gmail.com>
2025-02-10 14:51:11 -0300

    Update descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,46 +1,24 @@
-/* 
-- Downloader Ytmp4 By Izumi-kzx
-- https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
-*/
-import fetch from 'node-fetch';
+let handler = async (m, { conn, text }) => {
+    await m.react('🕓');
+    
+    if (!text) return conn.reply(m.chat, "❌ *Por favor, ingresa un enlace de YouTube.*", m);
 
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  await m.react('✖');
-  if (!text) throw Proporcióname el enlace de YouTube para que pueda ayudarte. 🎥;
+    try {
+        let url = `https://ytcdn.project-rian.my.id/download?url=${encodeURIComponent(text)}&resolution=360`;
+    
+        await conn.sendMessage(m.chat, { 
+            video: { url }, 
+            caption: "🎥 *Aquí está tu video*" 
+        }, { quoted: m });
 
-  await m.react('🕓');
-
-  try {
-    const apiKey = 'xenzpedo';
-    const response = await fetch(https://api.botcahx.eu.org/api/dowloader/yt?url=${encodeURIComponent(text)}&apikey=${apiKey});
-    const result = await response.json();
-
-    if (result.status && result.result && result.result.mp4) {
-      const { title, thumb, mp4, duration } = result.result;
-
-      await conn.sendMessage(
-        m.chat,
-        {
-          video: { url: mp4 },
-          mimetype: 'video/mp4',
-          caption: *🍟 Título:* ${title}\n*🍟 Duración:* ${Math.floor(duration / 60)}:${duration % 60} minutos,
-          thumbnail: await (await fetch(thumb)).buffer(),
-        },
-        { quoted: m }
-      );
-
-      await m.react('✅');
-    } else {
-      throw new Error('Error: No se pudo obtener el archivo MP4');
+        await m.react('✅');
+    } catch {
+        await m.react('❌');
+        conn.reply(m.chat, "*Hubo un error en la api*", m);
     }
-  } catch (error) {
-    await m.react('❌');
-    m.reply(❌ *Error:* ${error.message || 'Ocurrió un error desconocido'});
-  }
 };
 
-handler.help = ['ytmp4 <url>'];
-handler.command = ['ytmp4'];
-handler.tags = ['dl'];
-
+handler.help = ["ytmp4"];
+handler.tags = ["dl"];
+handler.command = ["ytmp4"];
 export default handler;
