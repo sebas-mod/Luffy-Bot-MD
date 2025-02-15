b71ef813cffd944d49c521c2299b3eb20fe42c74
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-02-15 01:39:06 -0300

    Update rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -2,21 +2,21 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
 let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('☁️ Ingresa la cantidad de *🤍 Corazones* que deseas Retirar.')
+if (!args[0]) return m.reply('☁️ Ingresa la cantidad de *☠️ Calaveras* que deseas Retirar.')
 if (args[0] == 'all') {
 let count = parseInt(user.bank)
 user.bank -= count * 1
 user.corazones += count * 1
-await m.reply(`☁️ Retiraste *${count} 🤍 Corazones* del Banco.`)
+await m.reply(`☁️ Retiraste *${count} ☠️ Calaveras* del Banco.`)
 return !0
 }
 if (!Number(args[0])) return m.reply('☁️ La cantidad deve ser un Numero.')
 let count = parseInt(args[0])
 if (!user.bank) return m.reply('No tienes *☁️ Corazones* en el Banco.')
-if (user.bank < count) return m.reply(`Solo tienes *${user.bank} 🤍 Corazones* en el Banco.`)
+if (user.bank < count) return m.reply(`Solo tienes *${user.bank} ☠️ Calaveras* en el Banco.`)
 user.bank -= count * 1
 user.corazones += count * 1
-await m.reply(`☁️ Retiraste *${count} 🤍 Corazones* del Banco.`)}
+await m.reply(`☁️ Retiraste *${count} ☠️ Calaveras* del Banco.`)}
 
 handler.help = ['retirar']
 handler.tags = ['rpg']
