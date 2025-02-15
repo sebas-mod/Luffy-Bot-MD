c0bcb7f2128cd9083a0fdc7a761ddf9d6fd1922d
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-02-15 01:23:55 -0300

    Update start-perfil.js

--- plugins/start-perfil.js
+++ plugins/start-perfil.js
@@ -10,13 +10,13 @@ let handler = async (m, { conn, usedPrefix, command}) => {
   let bio = await conn.fetchStatus(who).catch(_ => 'undefined')
   let biot = bio.status?.toString() || 'Sin Info'
   let user = global.db.data.users[who]
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://pomf2.lain.la/f/rycjgv2t.jpg')
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/0nMY9Y0/file.jpg')
   let { exp, corazones, name, registered, regTime, age, level } = global.db.data.users[who]
   let { min, xp, max } = xpRange(user.level, global.multiplier)
   let username = conn.getName(who)
   let prem = global.prems.includes(who.split`@`[0])
   let sn = createHash('md5').update(who).digest('hex')
-  let api = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
+  let api = await axios.get(`https://delirius-apiofc.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
   let userNationalityData = api.data.result
   let userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido'
   let img = await (await fetch(`${pp}`)).buffer()
