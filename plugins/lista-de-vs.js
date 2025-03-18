028322f6e14aba6071761de138b9c0f815c0105b
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-03-18 10:30:29 -0300

    Update lista-de-vs.js

--- plugins/lista-de-vs.js
+++ plugins/lista-de-vs.js
@@ -5,21 +5,31 @@
 */
 // [ 🍧 4VS4 FREE FIRE ]
 const partidas = {};
+
 const handler = async (m, { conn, args, command }) => {
   if (command === 'anotar') {
     const who = m.sender;
     const { name } = global.db.data.users[who];
     const partidaId = args[0];
+    
     if (!partidas[partidaId]) {
       conn.reply(m.chat, "No hay una partida activa en este momento.", m);
       return;
     }
+
     if (partidas[partidaId].jugadores.includes(name) || partidas[partidaId].suplentes.includes(name)) {
       conn.reply(m.chat, "¡Ya estás anotado en esta partida!", m);
       const mensaje = generarMensaje(partidas[partidaId]);
-      conn.sendMessage(m.chat, {text: mensaje, footer: "¡Anótate para el 4vs4!", buttons: [{buttonId: .anotar ${partidaId}, buttonText: {displayText: "📌 Anotar"}}], viewOnce: true, headerType: 1}, {quoted: m});
+      conn.sendMessage(m.chat, { 
+        text: mensaje, 
+        footer: "¡Anótate para el 4vs4!", 
+        buttons: [{ buttonId: `anotar ${partidaId}`, buttonText: { displayText: "📌 Anotar" } }], 
+        viewOnce: true, 
+        headerType: 1 
+      }, { quoted: m });
       return;
     }
+
     if (partidas[partidaId].jugadores.length < 4) {
       partidas[partidaId].jugadores.push(name);
     } else if (partidas[partidaId].suplentes.length < 2) {
@@ -29,28 +39,40 @@ const handler = async (m, { conn, args, command }) => {
       conn.sendMessage(m.chat, "Lista llena, suerte en el VS!", m);
       return;
     }
+
     if (partidas[partidaId].jugadores.length === 4 && partidas[partidaId].suplentes.length === 2) {
       conn.reply(m.chat, "¡Lista llena, suerte en el VS!", m);
     }
+
     const mensaje = generarMensaje(partidas[partidaId]);
-    conn.sendMessage(m.chat, {text: mensaje, footer: "¡Anótate para el 4vs4!", buttons: [{buttonId: .anotar ${partidaId}, buttonText: {displayText: "📌 Anotar"}}], viewOnce: true, headerType: 1}, {quoted: m});
+    conn.sendMessage(m.chat, { 
+      text: mensaje, 
+      footer: "¡Anótate para el 4vs4!", 
+      buttons: [{ buttonId: `anotar ${partidaId}`, buttonText: { displayText: "📌 Anotar" } }], 
+      viewOnce: true, 
+      headerType: 1 
+    }, { quoted: m });
     return;
   }
+
   if (args.length < 4) {
-    conn.reply(m.chat, 'Debes proporcionar esto.\n*.4vs4 <región> <hora> <Bandera> <modalidad>\n\n*Regiones\nSR (Sudamérica)\nEU (Estados Unidos)\n\n*Ejemplo:*\n.4vs4 SR 22:00 🇦🇷 infinito\n.4vs4 SR 22:00 🇦🇷 vivido\n.4vs4 EU 20:00 🇲🇽 infinito\n.4vs4 EU 20:00 🇲🇽 vivido', m);
+    conn.reply(m.chat, 'Debes proporcionar esto.\n*.4vs4 <región> <hora> <Bandera> <modalidad>\n\n*Regiones*\nSR (Sudamérica)\nEU (Estados Unidos)\n\n*Ejemplo:*\n.4vs4 SR 22:00 🇦🇷 infinito\n.4vs4 SR 22:00 🇦🇷 vivido\n.4vs4 EU 20:00 🇲🇽 infinito\n.4vs4 EU 20:00 🇲🇽 vivido', m);
     return;
   }
+
   const modalidad = args[3].toLowerCase();
   if (modalidad !== 'infinito' && modalidad !== 'vivido') {
     conn.reply(m.chat, 'Modalidad no válida. Escribe "infinito" o "vivido".', m);
     return;
   }
+
   const region = args[0].toUpperCase();
   if (region !== 'SR' && region !== 'EU') {
     conn.reply(m.chat, 'La región no es válida. Usa SR o EU.', m);
     return;
   }
-  const partidaId = ${m.chat}-${args[0]}-${args[1]};
+
+  const partidaId = `${m.chat}-${args[0]}-${args[1]}`;
   const horariosSR = { BO: "21:00", PE: "20:00", AR: "22:00" };
   let horariosEU = { CO: "21:00", MX: "" };
   if (region === 'EU') {
@@ -73,25 +95,33 @@ const handler = async (m, { conn, args, command }) => {
   }
 
   const mensaje = generarMensaje(partidas[partidaId]);
-  conn.sendMessage(m.chat, {text: mensaje, footer: "¡Anótate para el 4vs4!", buttons: [{buttonId: .anotar ${partidaId}, buttonText: {displayText: "📌 Anotar"}}], viewOnce: true, headerType: 1}, {quoted: m});
+  conn.sendMessage(m.chat, { 
+    text: mensaje, 
+    footer: "¡Anótate para el 4vs4!", 
+    buttons: [{ buttonId: `anotar ${partidaId}`, buttonText: { displayText: "📌 Anotar" } }], 
+    viewOnce: true, 
+    headerType: 1 
+  }, { quoted: m });
 };
 
+// Función para Generar el Mensaje
 function generarMensaje(partida) {
   const horarios = Object.entries(partida.horarios)
     .map(([pais, hora]) => {
-      const bandera = {BO: "🇧🇴", PE: "🇵🇪", AR: "🇦🇷", CO: "🇨🇴", MX: "🇲🇽"}[pais];
-      return *${bandera} ${pais} :* ${hora};
+      const bandera = { BO: "🇧🇴", PE: "🇵🇪", AR: "🇦🇷", CO: "🇨🇴", MX: "🇲🇽" }[pais];
+      return `${bandera} ${pais} : ${hora}`;
     })
     .join("\n");
 
-  const escuadra = [🥷 ${partida.jugadores[0] || ""}, 🥷 ${partida.jugadores[1] || ""}, 🥷 ${partida.jugadores[2] || ""}, 🥷 ${partida.jugadores[3] || ""}].join("\n");
-  const suplentes = [🥷 ${partida.suplentes[0] || ""}, 🥷 ${partida.suplentes[1] || ""}].join("\n");
+  const escuadra = [`🥷 ${partida.jugadores[0] || ""}`, `🥷 ${partida.jugadores[1] || ""}`, `🥷 ${partida.jugadores[2] || ""}`, `🥷 ${partida.jugadores[3] || ""}`].join("\n");
+  const suplentes = [`🥷 ${partida.suplentes[0] || ""}`, `🥷 ${partida.suplentes[1] || ""}`].join("\n");
 
-  return *4 VERSUS 4 ${partida.modalidad}*\n${horarios}\n*REGLAS:* ${partida.reglas}\n𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔\n${escuadra}\n𝗦𝗨𝗣𝗟𝗘𝗡𝗧𝗘𝗦\n${suplentes}.trim();
+  return `*4 VERSUS 4 ${partida.modalidad}*\n${horarios}\n*REGLAS:* ${partida.reglas}\n𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔\n${escuadra}\n𝗦𝗨𝗣𝗟𝗘𝗡𝗧𝗘𝗦\n${suplentes}`.trim();
 }
 
-handler.help = ['4vs4 <Reg|Hr|Bnd|Mod>']
-handler.tags = ['main']
+handler.help = ['4vs4 <Reg|Hr|Bnd|Mod>'];
+handler.tags = ['main'];
 handler.command = /^(4vs4|anotar)$/i;
-handler.group = true
+handler.group = true;
+
 export default handler;
