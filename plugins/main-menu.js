f428487f5608e585039bf23f6e1dc7f63efa3a22
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-04-04 15:43:02 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -224,7 +224,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
 
-    let img = 'https://files.fm/u/8p4ub48qe2'
+    let img = 'https://qu.ax/YEHhR.jpg'
     await m.react('🏴‍☠️')
     await conn.sendMessage(m.chat, {
       image: { url: img },
