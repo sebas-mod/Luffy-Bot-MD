5d64b5d68e9fc192f64da82f0a7c2b02d637b862
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-04-19 18:01:17 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,75 +1,9 @@
-import fetch from 'node-fetch';
+let handler = async (m, { conn, usedPrefix, isOwner }) => {
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝐒𝐞𝐛𝐚𝐬-𝐌𝐃;;\nFN:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nORG:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nTITLE:\nitem1.TEL;waid=5491166887146:5491166887146\nitem1.X-ABLabel:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: '𝐒𝐞𝐛𝐚𝐬-𝐌𝐃', contacts: [{ vcard }] }}, {quoted: m})
+}
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
 
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-    await m.react('🎉');
-
-    // Mensaje falso de contacto para citar
-    let fkontak = { 
-        "key": { 
-            "participants": "0@s.whatsapp.net", 
-            "remoteJid": "status@broadcast", 
-            "fromMe": false, 
-            "id": "Halo" 
-        }, 
-        "message": { 
-            "contactMessage": { 
-                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
-            }
-        }, 
-        "participant": "0@s.whatsapp.net" 
-    };
-
-    let username = await conn.getName(m.sender);
-
-    // vCard del creador
-    let vcard = `BEGIN:VCARD
-VERSION:3.0
-N:Dzn;Sebas
-FN:Sebas Dzn
-NICKNAME:Sebas-dzn
-ORG:sebas-MD
-TITLE:Soft
-item1.TEL;waid=5491166887146:+5491166887146
-item1.X-ABLabel:WhatsApp Owner
-item2.URL:https://github.com/sebas-MD
-item2.X-ABLabel:More
-item3.EMAIL;type=INTERNET:siloleeseresputo@gmail.com
-item3.X-ABLabel:Correo soporte
-item4.ADR:;;Argentina;;;;
-item4.X-ABLabel:Localización
-BDAY:2004-07-02
-END:VCARD`;
-
-    // Enviar contacto
-    await conn.sendMessage(m.chat, { 
-        contacts: { 
-            displayName: "Creador",
-            contacts: [{ vcard }] 
-        }
-    }, { quoted: fkontak });
-
-    // Enviar mensaje adicional con botón
-    let txt = `👋 *Hola \`${username}\`, este es el contacto de mi creador*`;
-
-    await conn.sendMessage(m.chat, {
-        text: txt,
-        footer: '© ᥴrᥱᥲძ᥆r ᥆𝖿іᥴіᥲᥣ sebas.MD',
-        buttons: [
-            {
-                buttonId: ".menu",
-                buttonText: {
-                    displayText: 'MENU BOT'
-                },
-                type: 1
-            }
-        ],
-        viewOnce: true,
-        headerType: 1
-    }, { quoted: m });
-};
-
-handler.help = ['owner', 'creator'];
-handler.tags = ['main'];
-handler.command = /^(owner|creator|creador|dueño)$/i;
-
-export default handler;
+export default handler
