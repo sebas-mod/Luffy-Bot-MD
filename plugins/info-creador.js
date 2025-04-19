48ae2f36b6ba8709907d7915368958def627b135
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-04-19 18:02:36 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝐒𝐞𝐛𝐚𝐬-𝐌𝐃;;\nFN:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nORG:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nTITLE:\nitem1.TEL;waid=5491166887146:5491166887146\nitem1.X-ABLabel:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝐒𝐞𝐛𝐚𝐬-𝐌𝐃;;\nFN:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nORG:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nTITLE:\nitem1.TEL;waid=+5491166887146:549 11 6688 7146\nitem1.X-ABLabel:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝐒𝐞𝐛𝐚𝐬-𝐌𝐃\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: '𝐒𝐞𝐛𝐚𝐬-𝐌𝐃', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
