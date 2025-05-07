2d5be05015184d09b72d359411dcf386e590dc2e
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-05-07 14:18:44 -0300

    Update toanime.js

--- plugins/toanime.js
+++ plugins/toanime.js
@@ -1,42 +1,30 @@
-const axios = require('axios');
-const Jimp = require('jimp');
-const FormData = require('form-data');
-const { baseUrl } = require('../config.js')
-
-/**Scraper Form 
-https://tools.betabotz.org
-**/
-
-
-async function toanime(input) {
-  const image = await Jimp.read(input);
-  const buffer = await new Promise((resolve, reject) => {
-    image.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
-      if (err) {
-        reject('Terjadi Error Saat Mengambil Data......');
-      } else {
-        resolve(buf);
-      }
-    });
-  });
-  const form = new FormData();
-  form.append('image', buffer, { filename: 'toanime.jpg' });
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m, {conn, text, args, usedPrefix, command}) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || q.mediaType || '';
+  if (!/image/g.test(mime)) throw '🛑 *Responda a una imagen*';
+  m.reply('☄️ *Conviertiendo la imagen en anime, espere un momento...*');
+  const data = await q.download?.();
+  const image = await uploadImage(data);
   try {
-    const { data } = await axios.post(`${baseUrl}/ai/toanime`, form, {
-      headers: {
-        ...form.getHeaders(),
-        'accept': 'application/json',
-      },
-    });
-    var res = {
-      image_data: data.result,
-      image_size: data.size
-    };
-    return res;
-  } catch (error) {
-    console.error('Identifikasi Gagal:', error);
-    return 'Identifikasi Gagal';
+    const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
+    await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
+  } catch (i) {
+    try {
+      const anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`;
+      await conn.sendFile(m.chat, anime2, 'error.jpg', null, m);
+    } catch (a) {
+      try {
+        const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
+        await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
+      } catch (e) {
+        throw '🛑 *Ocurrió un error*';
+      }
+    }
   }
-}
-
-module.exports = { toanime }
+};
+handler.help = ['toanime'];
+handler.tags = ['tools'];
+handler.register = true
+handler.command = ['jadianime','toanime'];
+export default handler;
