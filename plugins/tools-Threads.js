8f5a5bb6ede92b55b6e555cdd88bcaf0be86fbe7
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-05-07 13:54:01 -0300

    Update tools-Threads.js

--- plugins/tools-Threads.js
+++ plugins/tools-Threads.js
@@ -1,38 +1,28 @@
 import axios from 'axios';
-import FormData from 'form-data';
 
-async function Uguu(buffer, filename) {
-  let form = new FormData();
-  form.append('files[]', buffer, { filename });
-  let { data } = await axios.post('https://uguu.se/upload.php', form, {
-    headers: form.getHeaders()
-  });
-  if (data?.files?.[0]?.url) return data.files[0].url;
-  else throw m.reply('Error al subir imagen.');
-}
-
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, text }) => {
+  if (!text) return m.reply('Ingresa el link Threads!');
   try {
-    await m.react('⌛');
-    let type = (args[0] === 'mild') ? 'mild' : 'unblur';
-    let q = m.quoted ? m.quoted : m;
-    let mime = (q.msg || q).mimetype || '';
-    if (!mime.startsWith('image/')) throw m.reply('Responde a una imagen.');
-    let media = await q.download();
-    let url = await Uguu(media, image.${mime.split('/')[1]});
-    let { data } = await axios.get(https://abella.icu/unblur?url=${encodeURIComponent(url)}&type=${type}, {
-      responseType: 'arraybuffer'
-    });
-    await m.react('✅');
-    await conn.sendMessage(m.chat, { image: Buffer.from(data) }, { quoted: m });
+    let { data } = await axios.get('https://www.abella.icu/dl-threads?url=' + encodeURIComponent(text));
+    if (data.status !== 'success') throw m.reply('No se encontró la data!');
+    if (data.data.type === 'image') {
+      for (let img of data.data.urls) {
+        await conn.sendMessage(m.chat, { image: { url: img } }, { quoted: m });
+      }
+    } else if (data.data.type === 'video') {
+      for (let vid of data.data.urls) {
+        await conn.sendMessage(m.chat, { video: { url: vid.download_url } }, { quoted: m });
+      }
+    } else {
+      m.reply('El tipo no coincide con la deacarga');
+    }
   } catch (e) {
-    await m.react('❌');
-    m.reply(${e});
+    m.reply('Error : ' + e);
   }
 };
 
-handler.help = ['unblur', 'unblur mild'];
-handler.tags = ['tools'];
-handler.command = ['unblur'];
+handler.help = ['threads'].map(v => v + ' ');
+handler.command = ['threads'];
+handler.tags = ['descargas']
 
 export default handler;
